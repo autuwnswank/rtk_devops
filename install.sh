55dbf1362bbfa1004c0bc7808cd2bfa2c295ffaa
autumnswank <autumnswank1995@gmail.com>
2026-09-26 00:03:02 +0300

    Updating an installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget https://hashicorp-releases.yandexcloud.net/nomad/2.0.3/nomad_2.0.3_linux_amd64.zip;
-wget https://hashicorp-releases.yandexcloud.net/consul/2.0.3/consul_2.0.3_linux_amd64.zip;
+wget https://hashicorp-releases.yandexcloud.net/nomad/1.11.3/nomad_1.11.3_linux_amd64.zip;
+wget https://hashicorp-releases.yandexcloud.net/consul/1.22.7/consul_1.22.7_linux_amd64.zip;
 #готовим системные службы и конфиги
 sudo touch /etc/systemd/system/nomad.service; sudo touch /etc/systemd/system/consul.service;
 sudo mkdir -p /etc/nomad.d/; sudo cp ./templates/nomad.hcl /etc/nomad.d/nomad.hcl;
@@ -8,11 +8,11 @@ sudo mkdir -p /etc/consul.d/; sudo cp ./templates/consul.hcl /etc/consul.d/consu
 sudo cp ./templates/nomad.service /etc/systemd/system/nomad.service;
 sudo cp ./templates/consul.service /etc/systemd/system/consul.service;
 #установка nomad
-unzip nomad_2.0.3_linux_amd64.zip;
+unzip nomad_1.11.3_linux_amd64.zip;
 sudo mv nomad /usr/local/bin/;
 sudo chmod +x /usr/local/bin/nomad;
 #установка nomad
-unzip consul_2.0.3_linux_amd64.zip;
+unzip consul_1.22.7_linux_amd64.zip;
 sudo mv consul /usr/local/bin/;
 sudo chmod +x /usr/local/bin/consul;
 sudo systemctl enable nomad.service;
@@ -24,4 +24,7 @@ sudo systemctl restart consul.service;
 consul version;
 nomad version;
 sudo systemctl status nomad.service consul.service;
+#удаление бинарей в текущей директории
+sudo rm nomad_1.11.3_linux_amd64.zip;
+sudo rm consul_1.22.7_linux_amd64.zip;
 
